a9f5c8e46edd681e032dcdaf21d594edd3fabc7b
後藤　颯太 <sota@MacBook-Pro-2.local>
2020-09-14 13:50:47 +0900

    エラーメッセージ日本語化#7

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,9 +1,11 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-  before_action :check_guest, only: %i[update destroy]
+  before_action :check_guest, only: :destroy
 
   def check_guest
     if resource.email == 'guest@example.com'
-      redirect_to root_path, alert: 'ゲストユーザーの変更・削除はできません。'
+      redirect_to root_path, alert: 'ゲストユーザーは削除できません。'
     end
   end
+
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+        
+  validates :name, presence: true, length: { maximum: 8 }
+               
+    
   def self.guest
     find_or_create_by!(email: 'guest@example.com') do |user|
       user.password = SecureRandom.urlsafe_base64
     end
   end
+
+
+    
 end
