d969a8985c7c7f74c2e9eddfabb62dba8daa9c1f
後藤　颯太 <sota@MacBook-Pro-2.local>
2020-10-22 22:41:08 +0900

    コメント機能#20

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,25 +1,23 @@
 class CommentsController < ApplicationController
-    def create
-        @post = Post.find(params[:post_id])
-        @comment = @post.comments.build(comment_params)
-        @comment.user_id = current_user.id
-        if @comment.save
-        render :index
-        end
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.build(comment_params)
+    @comment.user_id = current_user.id
+    if @comment.save
+      render :index
     end
+  end
 
-    def destroy
-        @comment = Comment.find(params[:id])
-        if @comment.destroy
-        render :index
-        end
+  def destroy
+    @comment = Comment.find(params[:id])
+    if @comment.destroy
+      render :index
     end
+  end
 
-
-
-    private
+  private
 
     def comment_params
-        params.require(:comment).permit(:content, :post_id, :user_id)
+      params.require(:comment).permit(:content, :post_id, :user_id)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -22,12 +22,10 @@ Rails.application.routes.draw do
   get 'posts/:id/edit' => 'posts#edit'
   patch 'posts/:id' => 'posts#update', as: 'post'
   post 'posts/:id/destroy' => 'posts#destroy'
-  
+
   resources :users, only: %i[show index]
   resources :posts do
     resource :favorites, only: %i[create destroy]
-    resources :comments, only: [:create, :destroy]
+    resources :comments, only: %i[create destroy]
   end
-
-  
 end
