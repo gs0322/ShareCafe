4550ff8959a1e794939f6fd54af04f0f4659efc6
後藤　颯太 <sota@MacBook-Pro-2.local>
2020-10-23 14:38:23 +0900

    地図から探す&検索機能作成#22

---
+++ app/controllers/maps_controller.rb
@@ -0,0 +1,14 @@
+class MapsController < ApplicationController
+
+before_action :set_search
+
+    def set_search
+      @search = Post.ransack(params[:q])
+      @search_posts = @search.result(created_at: :desc).page(params[:page]).per(5)
+    end    
+    
+    def index
+        @posts=Post.all
+    end
+
+end
