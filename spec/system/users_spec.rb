a47811531f1af9abae6c91b8a93b034b13452034
後藤　颯太 <sota@MacBook-Pro-2.local>
2020-11-10 15:41:24 +0900

    ログインテスト作成#34

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -2,15 +2,21 @@ require 'rails_helper'
 
 RSpec.describe "User", type: :system do  
     
-    context 'ユーザー登録前' do
         it 'ユーザー登録ができるか' do
             visit   new_user_registration_path
-            fill_in "user_name", with: "山田太郎"
-            fill_in "user_email", with: "a@example.com"
-            fill_in "user_password", with: "123456"
-            fill_in "user_password_confirmation", with: "123456"
+            fill_in "user_name", with: "sota"
+            fill_in "user_email", with:  "tester@example.com"
+            fill_in "user_password", with: "password"
+            fill_in "user_password_confirmation", with: "password"
             find('#hoge').click
             expect(page).to have_content '新規登録が完了しました。'
         end
+        it "ログインするとTOP画面に遷移する" do
+            user = FactoryBot.create(:user)
+            visit root_path
+            find('#hoge').click
+            fill_in "user_email", with:  "tester@example.com"
+            fill_in "user_password", with: "password"
+            click_link "ログイン"
+        end
     end
-end
